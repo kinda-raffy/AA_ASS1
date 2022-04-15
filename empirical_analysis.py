27b6401c914997168f5aceb10abda33a5a5ebd6a
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-04-15 22:36:11 +1000

    Cleaned up empirical_analysis.py

--- empirical_analysis.py
+++ empirical_analysis.py
@@ -1,90 +1,59 @@
-import sys
 import time
-from dictionary.node import Node
+import tabulate
+import numpy as np
 from dictionary.word_frequency import WordFrequency
-from dictionary.base_dictionary import BaseDictionary
 from dictionary.list_dictionary import ListDictionary
 from dictionary.hashtable_dictionary import HashTableDictionary
 from dictionary.ternarysearchtree_dictionary import TernarySearchTreeDictionary
 
 
-def usage():
-    """
-    Print help/usage message.
-    """
-    print('python3 dictionary_file_based.py', '<approach> [data fileName] [command fileName] [output fileName]')
-    print('<approach> = <list | hashtable | tst>')
-    sys.exit(1)
-
-
-if __name__ == '__main__':
+def run_analysis():
     # Read in sampleData200k.txt
     dictionaries = {}
     with open('sampleData200k.txt', 'r') as f:
-        sampleData = f.readlines()
-        sampleData = [line.strip() for line in sampleData]
-        sampleData = [WordFrequency(word=line.split()[0], frequency=int(line.split()[1])) for line in sampleData]
-        
+        sample_data = [line.strip() for line in f.readlines()]
+        sample_data = [WordFrequency(word=line.split()[0], frequency=int(line.split()[1])) for line in sample_data]
+
         for i in range(16):
             prev_size = 200000 // 2 ** (16 - (i - 1))
             size = 200000 // 2 ** (16 - i)
-            dictionaries[str(size + prev_size)] = sampleData[:size + prev_size]
+            dictionaries[str(size + prev_size)] = sample_data[:size + prev_size]
 
+    NO_RUNS = 3
     # Build dictionaries.
-    list_times = []
-    hash_times = []
-    tst_times = []
-    dict_sizes = []
-    listDictionary = ListDictionary()
-    hashDictionary = HashTableDictionary()
-    tstDictionary = TernarySearchTreeDictionary()
-    iterations = 10
-    for key, value in dictionaries.items():
-        print('Building dictionaries with size:', key)
-        dict_sizes.append(key)
-        start_time = time.perf_counter_ns()
-        listDictionary.build_dictionary(value)
-        end_time = time.perf_counter_ns()
-        list_times.append(end_time - start_time)
-
-        start_time = time.perf_counter_ns()
-        hashDictionary.build_dictionary(value)
-        end_time = time.perf_counter_ns()
-        hash_times.append(end_time - start_time)
-
-        start_time = time.perf_counter_ns()
-        tstDictionary.build_dictionary(value)
-        end_time = time.perf_counter_ns()
-        tst_times.append(end_time - start_time)
-
-    for i in range(iterations-1):
-        print('Iteration', i+2)
-        for key, value in dictionaries.items():
-            iter = dict_sizes.index(key)
-            start_time = time.perf_counter_ns()
-            listDictionary.build_dictionary(value)
-            end_time = time.perf_counter_ns()
-            list_times[iter] = (list_times[iter] + end_time - start_time)
+    dict_sizes, list_times, hash_times, tst_times = list(), list(), list(), list()
+    list_dictionary, hash_dict, tst_dict = ListDictionary(), HashTableDictionary(), TernarySearchTreeDictionary()
+    # Calculate time taken to build dictionaries.
+    for run_no in range(NO_RUNS):
+        print(f'Run #{run_no + 1}')
+        for index, size in enumerate(dictionaries.keys()):
+            print('\tBuilding dictionary of size {}...'.format(size))
 
             start_time = time.perf_counter_ns()
-            hashDictionary.build_dictionary(value)
-            end_time = time.perf_counter_ns()
-            hash_times[iter] = (hash_times[iter] + end_time - start_time)
+            list_dictionary.build_dictionary(dictionaries[size])
+            list_times.append(list()) if run_no == 0 else None
+            list_times[index].append(time.perf_counter_ns() - start_time)
 
             start_time = time.perf_counter_ns()
-            tstDictionary.build_dictionary(value)
-            end_time = time.perf_counter_ns()
-            tst_times[iter] = (tst_times[iter] + end_time - start_time)
+            hash_dict.build_dictionary(dictionaries[size])
+            hash_times.append(list()) if run_no == 0 else None
+            hash_times[index].append(time.perf_counter_ns() - start_time)
 
-    list_times = [i/iterations for i in list_times]
-    hash_times = [i/iterations for i in hash_times]
-    tst_times  = [i/iterations for i in  tst_times]
-    list_times.pop(0)
-    hash_times.pop(0)
-    tst_times.pop(0)
-    dict_sizes.pop(0)
+            start_time = time.perf_counter_ns()
+            tst_dict.build_dictionary(dictionaries[size])
+            tst_times.append(list()) if run_no == 0 else None
+            tst_times[index].append(time.perf_counter_ns() - start_time)
 
-    print(dict_sizes)
-    print(list_times)
-    print(hash_times)
-    print(tst_times)
+            dict_sizes.append(size) if run_no == 0 else None
+        print()
+    # Calculate average times.
+    list_avg = [sum(times) / len(times) for times in list_times]
+    hash_avg = [sum(times) / len(times) for times in hash_times]
+    tst_avg = [sum(times) / len(times) for times in tst_times]
+    # Print results.
+    print(tabulate.tabulate(np.c_[dict_sizes, list_avg, hash_avg, tst_avg],
+                            headers=['Dictionary Size', 'List', 'Hash Table', 'TST'], tablefmt='fancy_grid'))
+
+
+if __name__ == '__main__':
+    run_analysis()
